bb4d6edeae75f102b8b7140e7e3d2101b5f580ef
labradorjman <30303644+labradorjman@users.noreply.github.com>
2018-04-23 14:07:17 +1000

    UI

--- Source/Carpocalypse/Public/PlayerActor.h
+++ Source/Carpocalypse/Public/PlayerActor.h
@@ -1,33 +1,33 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "Runtime/Engine/Classes/GameFramework/Character.h"
-#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
-#include "GameFramework/Actor.h"
-#include "PlayerActor.generated.h"
-
-UCLASS()
-class CARPOCALYPSE_API APlayerActor : public AActor
-{
-	GENERATED_BODY()
-	
-
-public:	
-	// Sets default values for this actor's properties
-	APlayerActor();
-
-	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
-		FVector playerLocation;
-
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	
-};
+//// Fill out your copyright notice in the Description page of Project Settings.
+//
+//#pragma once
+//
+//#include "CoreMinimal.h"
+//#include "Runtime/Engine/Classes/GameFramework/Character.h"
+//#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+//#include "GameFramework/Actor.h"
+//#include "PlayerActor.generated.h"
+//
+//UCLASS()
+//class CARPOCALYPSE_API APlayerActor : public AActor
+//{
+//	GENERATED_BODY()
+//	
+//
+//public:	
+//	// Sets default values for this actor's properties
+//	APlayerActor();
+//
+//	UPROPERTY(BlueprintReadWrite, VisibleAnywhere)
+//		FVector playerLocation;
+//
+//
+//protected:
+//	// Called when the game starts or when spawned
+//	virtual void BeginPlay() override;
+//
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//	
+//};
